d2ab80b35e06e29111d6194cca7bbf8e5bba6baf
Mikita Tratseuski <nikita.traz@gmail.com>
2023-10-11 12:17:27 +0300

    Initial Commit

---
+++ simpleweatherapp/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  simpleweatherapp
+//
+//  Created by Mikita Tratseuski on 10.10.23.
+//
+
+import Foundation
+
+struct Keys {
+    static let weatherAPIKey = "ffe2368a43df5ce9818d9a503c22183c"
+}
